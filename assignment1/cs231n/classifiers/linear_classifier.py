372e25e287c7d7dcb5bd606572374ffac9aeccba
Ivan Savov <ivan.savov@gmail.com>
2021-04-08 04:54:11 -0400

    predict and grid-search code implemented

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -103,7 +103,8 @@ class LinearClassifier(object):
         ###########################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        scores = X.dot(self.W)
+        y_pred = np.argmax(scores, axis=1)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return y_pred
